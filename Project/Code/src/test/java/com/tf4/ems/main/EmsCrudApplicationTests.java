a7659a27862823e9be428e6d50d64252e9674e7d
Vinay070702 <vinay.karnati2000@gmail.com>
2023-06-30 11:50:44 +0530

    Upload code

---
+++ Project/Code/src/test/java/com/tf4/ems/main/EmsCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tf4.ems.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+	    classes = EmsCrudApplicationTests.class
+	)
+
+class EmsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
